7770eead7fb0199530d141176d10f1a7237c8d52
pat taylor <patrick.schneider9@web.de>
2023-06-30 12:17:04 +0200

    feat(main): implement game skeleton

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Runner")
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
