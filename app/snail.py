89d47182cec0287c61097615945d5dfcfcd3ee60
pat taylor <patrick.schneider9@web.de>
2023-07-02 11:24:41 +0200

    feat(snail): implement rectangles and adapt snail class

--- app/snail.py
+++ app/snail.py
@@ -1,15 +1,44 @@
+from functools import cached_property
+import pathlib
+
+import pygame
 from app.constants import GAME_WIDTH
 
 
 class Snail:
-    def __init__(self, x: int, velocity: int, rerun: bool = False) -> None:
-        self.x: int = x 
+    def __init__(
+        self,
+        imagePath: pathlib.Path,
+        relPlacement: tuple,
+        velocity: int,
+        relPlacementStr: str = "midbottom",
+        rerun: bool = False,
+    ) -> None:
+        self.imagePath: pathlib.Path = imagePath
+        self.relativePlacement = {relPlacementStr: relPlacement}
         self.velocity: int = velocity
-        self.rerun: bool = rerun 
+        self.rerun: bool = rerun
+
+    @property
+    def surface(self):
+        return pygame.image.load(self.imagePath)
+
+    @cached_property
+    def rectangle(self):
+        return self.surface.get_rect(**self.relativePlacement)
+
     
     @property
+    def x(self):
+        return self.rectangle.x
+    
+    @property
+    def y(self):
+        return self.rectangle.y
+
+    @property
     def move_x(self):
-        self.x -= self.velocity
-        if self.x < 0 and self.rerun:
-            self.x = GAME_WIDTH + 200
-        return self.x
+        self.rectangle.x -= self.velocity
+        if self.rectangle.right < 0 and self.rerun:
+            self.rectangle.left = GAME_WIDTH
+        return self.rectangle.x
--- main.py
+++ main.py
@@ -16,9 +16,17 @@ sky_surface = pygame.image.load(pathlib.Path("app/assets/graphics/Sky.png"))
 ground_surface = pygame.image.load(pathlib.Path("app/assets/graphics/ground.png"))
 text_surface = test_font.render("My game", False, "Black")
 
-snail_surface = pygame.image.load(pathlib.Path("app/assets/graphics/snail/snail1.png"))
-snail_x_pos = 600
-snail = Snail(x=snail_x_pos, velocity=4, rerun=True)
+snail = Snail(
+    imagePath=pathlib.Path("app/assets/graphics/snail/snail1.png"),
+    relPlacement=(600, GROUND_ZERO),
+    velocity=1,
+    rerun=True,
+)
+
+player_surf = pygame.image.load(
+    pathlib.Path("app/assets/graphics/Player/player_walk_1.png")
+)
+player_rect = player_surf.get_rect(midbottom=(80, GROUND_ZERO))
 
 if __name__ == "__main__":
     while True:
@@ -30,7 +38,6 @@ if __name__ == "__main__":
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, GROUND_ZERO))
         screen.blit(text_surface, (300, 50))
-        screen.blit(snail_surface, (snail.move_x, 250))
+        screen.blit(snail.surface, (snail.move_x, snail.y))
+        screen.blit(player_surf, player_rect)
         pygame.display.update()
-
-
